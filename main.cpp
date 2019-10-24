61266fa274f8cc263c652f516dcae592997e5e04
Jona Ruof <jona.ruof@uni-ulm.de>
2019-10-24 23:06:44 +0200

    working on ring allocator

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <unistd.h>
 #include <iostream>
 #include <string.h>
+#include <pthread.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <errno.h>
+
 #include <tuple>
 #include <vector>
+#include <cstring>
 #include <unordered_map>
 
 namespace shm {
@@ -14,11 +18,113 @@ namespace shm {
 
     uint8_t buf[256];
 
+    constexpr size_t maxSegmentSize = 8192;
+
+    struct stub {
+
+        pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+        size_t startOffset = 0;
+        size_t endOffset = 0;
+    };
+
+    struct seg {
+
+        int fd;
+        uint8_t* mem;
+        size_t size;
+        std::string name;
+        stub* stb;
+        
+        seg (const char* name, const size_t size) 
+            : fd{shm_open(name, O_RDWR, 0777)},
+              mem{nullptr},
+              size{size},
+              name{name} {
+
+            // check if we need a new segment
+            if (0 > fd) {
+                fd = shm_open(name, O_CREAT | O_RDWR, 0777);
+                if (0 > fd) {
+                    throw std::runtime_error(
+                            std::string("Creating shared memory segment \"")
+                            + std::string(name)
+                            + std::string("\" failed: ")
+                            + std::strerror(errno));
+                }
+                // creation done, resize shared memory segment
+                if (0 > ftruncate(fd, sizeof(stub) + size)) { 
+                    throw std::runtime_error(
+                            std::string("Resizing shared memory segment \"")
+                            + std::string(name)
+                            + std::string("\" to ")
+                            + std::to_string(size)
+                            + std::string(" bytes failed: ")
+                            + std::strerror(errno));
+                }
+                // map and initialize new memory management stub
+                mem = (uint8_t*) mmap(0, size,
+                        PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+                // get and initialize stub
+                stub* stb = (stub*)mem;
+                *stb = {};
+            } else {
+                // also map and get stub
+                mem = (uint8_t*) mmap(0, size,
+                        PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+                stub* stb = (stub*)mem;
+            }
+        }
+
+        uint8_t* alloc (size_t msgSize) { 
+
+            pthread_mutex_lock(&stb->mutex);
+
+            if (stb->endOffset > size) {
+                // trying to wrap over to start
+                if (msgSize < stb->startOffset) {
+                    stb->endOffset = 0;
+                } else if (size * 2 < maxSegmentSize) {
+                    ftruncate(fd, size * 2);
+                } else {
+                    throw std::bad_alloc();
+                }
+            } else if (stb->endOffset < stb->startOffset) {
+                // this is the wrapped situation
+                if (stb->startOffset - stb->endOffset < msgSize) {
+                    // not much we can do
+                    throw std::bad_alloc();
+                }
+            }
+
+            uint8_t* result = (uint8_t*)(mem + stb->endOffset);
+            stb->endOffset += msgSize;
+
+            pthread_mutex_unlock(&stb->mutex);
+
+            return result;
+        }
+
+        void free () { 
+
+            pthread_mutex_lock(&stb->mutex);
+
+            if (stb->startOffset < stb->endOffset) { 
+                uint32_t msgSize = *(uint32_t*)(mem + stb->startOffset);
+                stb->startOffset += sizeof(uint32_t) + msgSize;
+            } else if (stb->endOffset < stb->startOffset) {
+                // TODO tomorrow
+            }
+
+            pthread_mutex_unlock(&stb->mutex);
+        }
+    };
+
     struct send {
 
         std::unordered_map<std::string, size_t> dataOffsets;
 
-        send (def d) { 
+        send (std::string topic, def d) { 
 
             uint32_t msgSize = 0;
 
@@ -68,7 +174,7 @@ namespace shm {
                 *(uint32_t*)ptr = entryDataLen;
                 ptr += sizeof(uint32_t);
 
-                // store offset in buffer
+                // store offset to buffer start
                 dataOffsets[entryName] = ptr - buf;
                 ptr += entryDataLen;
             }
@@ -107,7 +213,7 @@ namespace shm {
                 uint32_t entryDataLen = *(uint32_t*)ptr;
                 ptr += sizeof(uint32_t);
 
-                // read data 
+                // read data offset relative to buffer start
                 dataOffsets[entryName] = ptr - buf;
                 ptr += entryDataLen;
             }
@@ -123,10 +229,10 @@ namespace shm {
 
 int main (int argc, char** argv) {
 
-    int fd = shm_open("test2", O_CREAT, 0777);
+    shm::seg segTest("segment_test", 256);
 
     {
-        shm::send msg({
+        shm::send msg("/spatzsim/test2", {
                 {"aaaa", sizeof(float)},
                 {"y", sizeof(float)},
                 {"z", sizeof(float)}
